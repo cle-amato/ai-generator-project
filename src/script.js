c07c84694468daf51dedef9f97479ef653d45919
cle-amato <cle.amato@gmail.com>
2024-01-27 12:57:58 +0000

    added JS submit handling

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Recipe here",
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
+let recipeFormELement = document.querySelector("#recipe-generator-form");
+
+recipeFormELement.addEventListener("submit", generateRecipe);
