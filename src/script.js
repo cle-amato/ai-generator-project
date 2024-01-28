0abdce9015cbc02fefad6cefa74af7c4d68ce2a6
cle-amato <cle.amato@gmail.com>
2024-01-28 18:33:17 +0000

    improved UX

--- src/script.js
+++ src/script.js
@@ -5,7 +5,6 @@ function displayRecipe(response) {
     cursor: null,
     delay: 1,
   });
-  console.log(response.data.answer);
 }
 
 function generateRecipe(event) {
@@ -15,15 +14,17 @@ function generateRecipe(event) {
   let userInputValue = userInputElement.value;
   let apiKey = "ca0094f431ob9cbfef2ed6ce95bt0cc7";
   let context =
-    "You are a food expert and you love to create simple and short recipes. Provide 3 ideas of recipe in a bullet point format in HTML.";
+    "You are a food expert and you love to create simple and short recipes. Provide 3 ideas of recipe in a bullet point format in HTML. Only provide titles, not ingredients or directions.";
   let prompt = `Provide 3 recipes titles that include ${userInputValue}`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let responseContainerElement = document.querySelector("#response-container");
+  responseContainerElement.classList.remove("hidden");
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = `<div class="blink">Generating response...</div>`;
+
   axios.get(apiUrl).then(displayRecipe);
-  console.log("Generating recipe");
-  console.log(context);
-  console.log(prompt);
 }
 
 let recipeFormELement = document.querySelector("#recipe-generator-form");
